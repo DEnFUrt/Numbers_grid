0d3d9bd599da62856676d0b007e108590fc2897d
DEnFUrt <musorov@gmail.com>
2019-12-05 22:30:31 +0500

    initial

---
+++ static/js/constants.js
@@ -0,0 +1,4 @@
+const btnsContainer = document.getElementById('btns');
+const colorField = document.getElementById('color-field');
+
+const panelsNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
