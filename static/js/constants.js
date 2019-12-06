65866e90759453a0217a35082eea3cd95d4d555c
DEnFUrt <musorov@gmail.com>
2019-12-06 18:42:07 +0500

    правим стили

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,4 +1,4 @@
-const btnsContainer = document.getElementById('btns');
-const colorField = document.getElementById('color-field');
+const btnsContainer = document.getElementById('container');
+const numbersField = document.getElementById('fieldNumber');
 
 const panelsNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,23 @@
-function createColorButtons(data, container) {
+function createNumbersButtons(data, container) {
     for(let row = 0; row < data.length; row++) {
-        const cells = data[row];
-
         const rowComponent = document.createElement('div');
         rowComponent.className = 'row';
-        container.appendChild(rowComponent);
+        btnsContainer.appendChild(rowComponent);
+        const button = document.createElement('button');
+        button.className = 'btn';
+        //button.style.backgroundColor = cells[cellId].color;
+        button.innerText = data[row];
 
-        for(let cellId = 0; cellId < cells.length; cellId++) {
-            const button = document.createElement('button');
+            // button.addEventListener('mouseenter', function() {
+            //     colorField.style.backgroundColor = cells[cellId].color;
+            // })
+            // button.addEventListener('mouseleave', function() {
+            //     colorField.style.backgroundColor = '#fff';
+            // })
 
-            button.className = 'btn';
-            button.style.backgroundColor = cells[cellId].color;
-            button.innerText = cells[cellId].name;
-
-            button.addEventListener('mouseenter', function() {
-                colorField.style.backgroundColor = cells[cellId].color;
-            })
-            button.addEventListener('mouseleave', function() {
-                colorField.style.backgroundColor = '#fff';
-            })
-
-            rowComponent.appendChild(button);
-        }
+        rowComponent.appendChild(button);
+        
     }
 }
 
-createColorButtons(panelsColor, btnsContainer);
+createNumbersButtons(panelsNumbers, btnsContainer);
